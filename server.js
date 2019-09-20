165e23883d68b830086a46a15efada389165668b
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-20 00:30:18 -0700

    article.js code added

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var expresshandlebars = ("expresshandlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+
+var db = require("./models")
